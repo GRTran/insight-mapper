158f4db1cf0d6cc1c7db58c122b470e5dc2cf272
David Clark <davidatfernlea1992@gmail.com>
2024-05-21 12:15:53 +0200

    Adding a test route, and middleware for back to front test

--- app/main.py
+++ app/main.py
@@ -8,8 +8,19 @@ from app.routers import postcodes
 from app.core.config import config
 from app.db.base import Base
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*",]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(postcodes.router)
 
 # Create the database tables - Only needs to be done once then it can be commented out
@@ -21,3 +32,21 @@ Base.metadata.create_all(
 @app.get("/")
 async def root():
     return {"message": "Hello root!"}
+
+
+@app.get("/get_coordinates")
+async def read_item():
+    return [
+         {
+            'geoCode': [48.86, 2.3522],
+            'description': 'Marker 1'
+        },
+        {
+            'geoCode': [48.85, 2.3522],
+            'description': 'Marker 2'
+        },
+        {
+            'geoCode': [48.855, 2.34],
+            'description': 'Marker 3'
+        }
+]
